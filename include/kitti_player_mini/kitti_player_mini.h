e1603babc51e6975b674b7999551281cbf22c35f
Lu Gan <ganlu@umich.edu>
2020-03-08 23:28:12 -0400

    cleaned code

--- include/kitti_player_mini/kitti_player_mini.h
+++ include/kitti_player_mini/kitti_player_mini.h
@@ -6,7 +6,6 @@
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 #include <Eigen/Geometry>
-//#include <opencv/cv.hpp>
 #include <opencv2/core/core.hpp>
 #include <pcl_conversions/pcl_conversions.h>
 #include <tf/transform_broadcaster.h>
@@ -28,21 +27,17 @@ class KittiPlayerMini{
       left_camera_pose_publisher_ = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("left_camera_pose", 1);
     }
 
-    void process_scan() {
-      int scan_num = 100;
+    void process_scans(std::string sequence_dir, int scan_num) {
       
-      std::string velodyne_dir = "/media/ganlu/PERL-SSD/Datasets/KITTI/dataset/sequences/05/velodyne/";
-      std::string left_color_image_dir = "/media/ganlu/PERL-SSD/Datasets/KITTI/dataset/sequences/05/image_2/";
-      std::string depth_image_dir = "/media/ganlu/PERL-SSD/Datasets/KITTI/dataset/sequences/05/depth_img/";
+      std::string velodyne_dir = sequence_dir + "velodyne/";
+      std::string left_color_image_dir = sequence_dir + "image_2/";
+      std::string depth_image_dir = sequence_dir + "depth_img/";
+      
       for (int scan_id = 0; scan_id <= scan_num; ++scan_id) {
-        char scan_id_c[256];
-        sprintf(scan_id_c, "%06d", scan_id);
-        std::string velodyne_name = velodyne_dir + std::string(scan_id_c) + ".bin";
-        velodyne_ = read_velodyne(velodyne_name);
+        
+        ros::Duration(3.5).sleep();
 
-        std::string left_color_image_name = left_color_image_dir + std::string(scan_id_c) + ".png";
-        std::string depth_image_name = depth_image_dir + std::string(scan_id_c) + ".png";
-
+        // Read pose
         Eigen::Matrix4d left_camera_pose = left_camera_poses_[scan_id];
         Eigen::Matrix4d init_trans_to_ground;
         init_trans_to_ground <<  1, 0, 0, 0,
@@ -50,107 +45,111 @@ class KittiPlayerMini{
                                  0,-1, 0, 1,
                                  0, 0, 0, 1;
         left_camera_pose = init_trans_to_ground * left_camera_pose;
+        tf::Transform tf_pose;
+        Eigen::Affine3d eigen_affine_pose(left_camera_pose);
+        tf::transformEigenToTF(eigen_affine_pose, tf_pose);
 
+        // Read calib
+        Eigen::Matrix4d Tr;
+        Tr << -1.857739385241e-03, -9.999659513510e-01, -8.039975204516e-03, -4.784029760483e-03,
+              -6.481465826011e-03, 8.051860151134e-03, -9.999466081774e-01, -7.337429464231e-02, 
+              9.999773098287e-01, -1.805528627661e-03, -6.496203536139e-03, -3.339968064433e-01,
+              0, 0, 0, 1;
+        tf::Transform tf_Tr;
+        Eigen::Affine3d eigen_affine_Tr(Tr);
+        tf::transformEigenToTF(eigen_affine_Tr, tf_Tr);
+      
+        char scan_id_c[256];
+        sprintf(scan_id_c, "%06d", scan_id);
         ros::Time t_cur = ros::Time::now();
         
-        // publish tf
-        tf::Transform transform;
-        Eigen::Affine3d eigen_affine_pose(left_camera_pose);
-        tf::transformEigenToTF(eigen_affine_pose, transform);
-        br_.sendTransform(tf::StampedTransform(transform, t_cur, "map", "robot"));
-        Eigen::Matrix4d Tr;
-        Tr << -0.001857739385241, -0.999965951350955, -0.008039975204516, -0.004784029760483,
-              -0.006481465826011,  0.008051860151134, -0.999946608177406, -0.073374294642306,
-               0.999977309828677, -0.001805528627661, -0.006496203536139, -0.333996806443304,
-       	       0                ,  0                ,  0                ,  1.000000000000000;
-        Eigen::Affine3d eigen_affine_Tr(Tr);
-        tf::transformEigenToTF(eigen_affine_Tr, transform);
-        br_.sendTransform(tf::StampedTransform(transform, t_cur, "robot", "sensor"));
-
-
-        // publish pose
-
-        // publish velodyne
+        // Read velodyne
+        std::string velodyne_name = velodyne_dir + std::string(scan_id_c) + ".bin";
+        velodyne_ = read_velodyne(velodyne_name);
         sensor_msgs::PointCloud2 cloud_msg;
         pcl::toROSMsg(*velodyne_, cloud_msg);
-        cloud_msg.header.frame_id = "sensor";
+        cloud_msg.header.frame_id = "velodyne";
         cloud_msg.header.stamp = t_cur;
-        velodyne_publisher_.publish(cloud_msg);
-        publish_posecov(eigen_affine_pose, t_cur);
 
-        // publish images
-        cv::Mat depth_image = cv::imread(depth_image_name, CV_LOAD_IMAGE_ANYDEPTH);
-        cv_bridge::CvImage depth_msg;
-        depth_msg.header.stamp = t_cur;
-        depth_msg.encoding = sensor_msgs::image_encodings::TYPE_16UC1;
-        depth_msg.image = depth_image;
-
+        // Read left color image
+        std::string left_color_image_name = left_color_image_dir + std::string(scan_id_c) + ".png";
         cv::Mat left_color_image = cv::imread(left_color_image_name, CV_LOAD_IMAGE_UNCHANGED);
         cv_bridge::CvImage left_color_msg;
-        left_color_msg.header.stamp = t_cur;
         left_color_msg.encoding = sensor_msgs::image_encodings::RGB8;
         left_color_msg.image = left_color_image;
+        left_color_msg.header.stamp = t_cur;
+        
+        // Read depth image
+        std::string depth_image_name = depth_image_dir + std::string(scan_id_c) + ".png";
+        cv::Mat depth_image = cv::imread(depth_image_name, CV_LOAD_IMAGE_ANYDEPTH);
+        cv_bridge::CvImage depth_msg;
+        depth_msg.encoding = sensor_msgs::image_encodings::TYPE_16UC1;
+        depth_msg.image = depth_image;
+        depth_msg.header.stamp = t_cur;
+ 
+        // publish tf
+        br_.sendTransform(tf::StampedTransform(tf_pose, t_cur, "map", "left_camera"));
+        br_.sendTransform(tf::StampedTransform(tf_Tr, t_cur, "left_camera", "velodyne"));
+       
+        // publish msgs
+        velodyne_publisher_.publish(cloud_msg);
         left_color_image_publisher_.publish(left_color_msg);
         depth_image_publisher_.publish(depth_msg);
+        publish_posecov(eigen_affine_pose, t_cur);
         
-        ros::Duration(3.5).sleep();
-      
+        std::cout << scan_id << std::endl;
       }
     }
 
-    void publish_posecov(Eigen::Affine3d pose, ros::Time t)
-{
-//    geometry_msgs::Pose pose_msg;
-//    tf::poseEigenToMsg(pose, pose_msg);
+    void publish_posecov(Eigen::Affine3d pose, ros::Time t) {
+      geometry_msgs::PoseWithCovarianceStamped pose_msg;
+      pose_msg.header.frame_id = "left_camera";
+      pose_msg.header.stamp = t;
 
-    geometry_msgs::PoseWithCovarianceStamped pose_msg;
-    pose_msg.header.frame_id = "robot";
-    pose_msg.header.stamp = t;
+      // set pose
+      tf::poseEigenToMsg(pose, pose_msg.pose.pose);
 
-    // set pose
-    tf::poseEigenToMsg(pose, pose_msg.pose.pose);
+      // set cov
+      pose_msg.pose.covariance[6*0+0] = 0.0;
+      pose_msg.pose.covariance[6*0+1] = 0.0;
+      pose_msg.pose.covariance[6*0+2] = 0.0;
+      pose_msg.pose.covariance[6*0+3] = 0.0;
+      pose_msg.pose.covariance[6*0+4] = 0.0;
+      pose_msg.pose.covariance[6*0+5] = 0.0;
+      pose_msg.pose.covariance[6*1+0] = 0.0;
+      pose_msg.pose.covariance[6*1+1] = 0.0;
+      pose_msg.pose.covariance[6*1+2] = 0.0;
+      pose_msg.pose.covariance[6*1+3] = 0.0;
+      pose_msg.pose.covariance[6*1+4] = 0.0;
+      pose_msg.pose.covariance[6*1+5] = 0.0;
+      pose_msg.pose.covariance[6*2+0] = 0.0;
+      pose_msg.pose.covariance[6*2+1] = 0.0;
+      pose_msg.pose.covariance[6*2+2] = 0.0;
+      pose_msg.pose.covariance[6*2+3] = 0.0;
+      pose_msg.pose.covariance[6*2+4] = 0.0;
+      pose_msg.pose.covariance[6*2+5] = 0.0;
+      pose_msg.pose.covariance[6*3+0] = 0.0;
+      pose_msg.pose.covariance[6*3+1] = 0.0;
+      pose_msg.pose.covariance[6*3+2] = 0.0;
+      pose_msg.pose.covariance[6*3+3] = 0.0;
+      pose_msg.pose.covariance[6*3+4] = 0.0;
+      pose_msg.pose.covariance[6*3+5] = 0.0;
+      pose_msg.pose.covariance[6*4+0] = 0.0;
+      pose_msg.pose.covariance[6*4+1] = 0.0;
+      pose_msg.pose.covariance[6*4+2] = 0.0;
+      pose_msg.pose.covariance[6*4+3] = 0.0;
+      pose_msg.pose.covariance[6*4+4] = 0.0;
+      pose_msg.pose.covariance[6*4+5] = 0.0;
+      pose_msg.pose.covariance[6*5+0] = 0.0;
+      pose_msg.pose.covariance[6*5+1] = 0.0;
+      pose_msg.pose.covariance[6*5+2] = 0.0;
+      pose_msg.pose.covariance[6*5+3] = 0.0;
+      pose_msg.pose.covariance[6*5+4] = 0.0;
+      pose_msg.pose.covariance[6*5+5] = 0.0;
 
-    // set cov
-    pose_msg.pose.covariance[6*0+0] = 0.0;
-    pose_msg.pose.covariance[6*0+1] = 0.0;
-    pose_msg.pose.covariance[6*0+2] = 0.0;
-    pose_msg.pose.covariance[6*0+3] = 0.0;
-    pose_msg.pose.covariance[6*0+4] = 0.0;
-    pose_msg.pose.covariance[6*0+5] = 0.0;
-    pose_msg.pose.covariance[6*1+0] = 0.0;
-    pose_msg.pose.covariance[6*1+1] = 0.0;
-    pose_msg.pose.covariance[6*1+2] = 0.0;
-    pose_msg.pose.covariance[6*1+3] = 0.0;
-    pose_msg.pose.covariance[6*1+4] = 0.0;
-    pose_msg.pose.covariance[6*1+5] = 0.0;
-    pose_msg.pose.covariance[6*2+0] = 0.0;
-    pose_msg.pose.covariance[6*2+1] = 0.0;
-    pose_msg.pose.covariance[6*2+2] = 0.0;
-    pose_msg.pose.covariance[6*2+3] = 0.0;
-    pose_msg.pose.covariance[6*2+4] = 0.0;
-    pose_msg.pose.covariance[6*2+5] = 0.0;
-    pose_msg.pose.covariance[6*3+0] = 0.0;
-    pose_msg.pose.covariance[6*3+1] = 0.0;
-    pose_msg.pose.covariance[6*3+2] = 0.0;
-    pose_msg.pose.covariance[6*3+3] = 0.0;
-    pose_msg.pose.covariance[6*3+4] = 0.0;
-    pose_msg.pose.covariance[6*3+5] = 0.0;
-    pose_msg.pose.covariance[6*4+0] = 0.0;
-    pose_msg.pose.covariance[6*4+1] = 0.0;
-    pose_msg.pose.covariance[6*4+2] = 0.0;
-    pose_msg.pose.covariance[6*4+3] = 0.0;
-    pose_msg.pose.covariance[6*4+4] = 0.0;
-    pose_msg.pose.covariance[6*4+5] = 0.0;
-    pose_msg.pose.covariance[6*5+0] = 0.0;
-    pose_msg.pose.covariance[6*5+1] = 0.0;
-    pose_msg.pose.covariance[6*5+2] = 0.0;
-    pose_msg.pose.covariance[6*5+3] = 0.0;
-    pose_msg.pose.covariance[6*5+4] = 0.0;
-    pose_msg.pose.covariance[6*5+5] = 0.0;
-
-    // publish
-    left_camera_pose_publisher_.publish(pose_msg);
-}
+      // publish
+      left_camera_pose_publisher_.publish(pose_msg);
+    }
         
     bool read_left_camera_poses(const std::string pose_name) {
       if (std::ifstream(pose_name)) {
--- src/kitti_player_mini_node.cpp
+++ src/kitti_player_mini_node.cpp
@@ -5,9 +5,13 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "kitti_player_mini_node");
   ros::NodeHandle nh("~");
 
+  int scan_num = 1100;
+  std::string sequence_dir = "/media/ganlu/PERL-SSD/Datasets/KITTI/dataset/sequences/06/";
+  
+
   KittiPlayerMini kitti_player_mini(nh);
-  kitti_player_mini.read_left_camera_poses("/media/ganlu/PERL-SSD/Datasets/KITTI/dataset/poses/05.txt");
-  kitti_player_mini.process_scan();
+  kitti_player_mini.read_left_camera_poses("/media/ganlu/PERL-SSD/Datasets/KITTI/dataset/poses/06.txt");
+  kitti_player_mini.process_scans(sequence_dir, scan_num);
   ros::spin();
 
   return 0;
